1dd3c2899e02bddd075f6a6269bd6ba7a03852d8
ikngtty <ikngtty@gmail.com>
2025-11-03 22:21:24 +0900

    feat: Manage command settings.

--- scripts/README.md
+++ scripts/README.md
@@ -1,6 +1,6 @@
 # Scripts
 
-To register a command to Discord.
+To manage command settings of Discord Application.
 
 ## Preparation
 
@@ -9,5 +9,7 @@ Set environment variables. (cf. [.env.sample](.env.sample))
 ## Run
 
 ```shell
-$ node index.js
+$ node index.js list
+$ node index.js add <command_name>
+$ node index.js remove <command_id>
 ```
--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,124 @@
 async function main() {
+  // Get env vars.
   const appId = requireEnvVar(process.env, "APP_ID");
   const botToken = requireEnvVar(process.env, "BOT_TOKEN");
   const guildId = requireEnvVar(process.env, "GUILD_ID");
 
-  const url = `https://discord.com/api/v10/applications/${appId}/guilds/${guildId}/commands`;
-  const body = {
-    name: "ping",
-    description: "🏓",
-  };
-  const headers = {
-    Authorization: `Bot ${botToken}`,
-    "Content-Type": "application/json",
-  };
+  // Get arguments.
+  if (process.argv.length <= 2) {
+    console.error("Required argument subcommand");
+    process.exit(1);
+  }
+  const subcommand = process.argv[2];
+
+  // Execute.
+  switch (subcommand) {
+    case "list":
+      await listCommand(appId, botToken, guildId);
+      break;
+
+    case "add":
+      if (process.argv.length <= 3) {
+        console.error("Required argument command name");
+        process.exit(1);
+      }
+      const commandNameToAdd = process.argv[3];
+      await addCommand(appId, botToken, guildId, commandNameToAdd);
+      break;
+
+    case "remove":
+      if (process.argv.length <= 3) {
+        console.error("Required argument command ID");
+        process.exit(1);
+      }
+      const commandIdToRemove = process.argv[3];
+      await removeCommand(appId, botToken, guildId, commandIdToRemove);
+      break;
+
+    default:
+      console.error("Invalid subcommand");
+      process.exit(1);
+      break;
+  }
+}
+
+main().catch(console.error);
+
+async function listCommand(appId, botToken, guildId) {
+  const url = getDicordApiUrlGuildCommands(appId, guildId);
+  const headers = makeHeaders(botToken);
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers,
+  });
+
+  await showResponse(response);
+}
+
+async function addCommand(appId, botToken, guildId, commandName) {
+  const url = getDicordApiUrlGuildCommands(appId, guildId);
+  const headers = makeHeaders(botToken);
+
+  let body = null;
+  switch (commandName) {
+    case "ping":
+      body = {
+        name: "ping",
+        description: "🏓",
+      };
+      break;
+
+    default:
+      console.error("Invalid command name");
+      process.exit(1);
+      break;
+  }
 
   const response = await fetch(url, {
     method: "POST",
     headers,
     body: JSON.stringify(body),
   });
-  console.log(`${response.status} ${response.statusText}`);
-  console.dir(await response.json(), { depth: null });
+
+  await showResponse(response);
 }
 
-main().catch(console.error);
+async function removeCommand(appId, botToken, guildId, commandId) {
+  const url = getDicordApiUrlGuildCommand(appId, guildId, commandId);
+  const headers = makeHeaders(botToken);
+
+  const response = await fetch(url, {
+    method: "DELETE",
+    headers,
+  });
+
+  await showResponse(response);
+}
+
+function getDicordApiUrlGuildCommands(appId, guildId) {
+  return `https://discord.com/api/v10/applications/${appId}/guilds/${guildId}/commands`;
+}
+
+function getDicordApiUrlGuildCommand(appId, guildId, commandId) {
+  const urlOfCommands = getDicordApiUrlGuildCommands(appId, guildId);
+  return `${urlOfCommands}/${commandId}`;
+}
+
+function makeHeaders(botToken) {
+  return {
+    Authorization: `Bot ${botToken}`,
+    "Content-Type": "application/json",
+  };
+}
+
+async function showResponse(response) {
+  console.log(`${response.status} ${response.statusText}`);
+  const body = await response.text();
+  if (body !== "") {
+    console.dir(JSON.parse(body), { depth: null });
+  }
+}
 
 function requireEnvVar(env, key) {
   const v = env[key];
