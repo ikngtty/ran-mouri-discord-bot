2241f093789b5a8be7ffeaa26ace52d4205dcdb1
ikngtty <ikngtty@gmail.com>
2025-11-07 01:05:00 +0900

    feat: Verifies a signature.

--- entrypoint/ran-mouri-discord-bot-entrypoint/src/index.ts
+++ entrypoint/ran-mouri-discord-bot-entrypoint/src/index.ts
@@ -11,20 +11,66 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { sign } from 'tweetnacl';
 import { ResponseError } from './types';
+
+const REQUEST_HEADER_SIGNATURE = 'X-Signature-Ed25519';
+const REQUEST_HEADER_TIMESTAMP = 'X-Signature-Timestamp';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+		// Get env vars.
 		const publicKey = env.DISCORD_PUBLIC_KEY;
 		if (publicKey == null || publicKey === '') {
 			console.log('Required env var "DISCORD_PUBLIC_KEY".');
-			const err: ResponseError = {
-				title: 'Internal Server Error',
-				detail: 'Initialization is failed.',
-			};
-			return new Response(JSON.stringify(err), { status: 500 });
+			return makeResponseNoEnvVar();
+		}
+
+		// Get request's headers and body.
+		const signature = request.headers.get(REQUEST_HEADER_SIGNATURE);
+		if (signature == null || signature === '') {
+			return makeResponseNoHeaderOfSign(REQUEST_HEADER_SIGNATURE);
+		}
+		const timestamp = request.headers.get(REQUEST_HEADER_TIMESTAMP);
+		if (timestamp == null || timestamp === '') {
+			return makeResponseNoHeaderOfSign(REQUEST_HEADER_TIMESTAMP);
+		}
+		const body = await request.text();
+
+		// Verify the signature.
+		if (!signatureIsValid(publicKey, body, timestamp, signature)) {
+			return makeResponseInvalidSignature();
 		}
 
 		return new Response('Hello World!');
 	},
 } satisfies ExportedHandler<Env>;
+
+function signatureIsValid(publicKey: string, body: string, timestamp: string, signature: string): boolean {
+	const message = body + timestamp;
+	return sign.detached.verify(Buffer.from(message), Buffer.from(signature, 'hex'), Buffer.from(publicKey, 'hex'));
+}
+
+function makeResponseNoEnvVar(): Response {
+	const err: ResponseError = {
+		title: 'Internal Server Error',
+		detail: 'Initialization is failed.',
+	};
+	return new Response(JSON.stringify(err), { status: 500 });
+}
+
+function makeResponseNoHeaderOfSign(key: string): Response {
+	const err: ResponseError = {
+		title: 'Unauthorized',
+		detail: `Header key "${key}" is required but missing.`,
+	};
+	return new Response(JSON.stringify(err), { status: 401 });
+}
+
+function makeResponseInvalidSignature(): Response {
+	const err: ResponseError = {
+		title: 'Unauthorized',
+		detail: 'Your signature is invalid.',
+	};
+	return new Response(JSON.stringify(err), { status: 401 });
+}
--- entrypoint/ran-mouri-discord-bot-entrypoint/test/index.spec.ts
+++ entrypoint/ran-mouri-discord-bot-entrypoint/test/index.spec.ts
@@ -1,6 +1,9 @@
 import { createExecutionContext, waitOnExecutionContext } from 'cloudflare:test';
+import { sign } from 'tweetnacl';
 import { describe, it, expect } from 'vitest';
 import worker from '../src/index';
+
+const SEED = Buffer.from('SeedForTest234567890123456789012');
 
 // For now, you'll need to do something like this to get a correctly-typed
 // `Request` to pass to `worker.fetch()`.
@@ -8,8 +11,19 @@ const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
 describe('The Entrypoint Worker', () => {
 	it('responds with Hello World!', async () => {
-		const request = new IncomingRequest('http://example.com');
-		const env = makeEnv();
+		const body = 'text from discord';
+		const timestamp = '12345';
+		const { publicKey, signature } = makeSignature(body, timestamp);
+
+		const request = new IncomingRequest('http://example.com', {
+			method: 'POST',
+			headers: {
+				'X-Signature-Ed25519': signature,
+				'X-Signature-Timestamp': timestamp,
+			},
+			body,
+		});
+		const env = { DISCORD_PUBLIC_KEY: publicKey };
 		// Create an empty context to pass to `worker.fetch()`.
 		const ctx = createExecutionContext();
 
@@ -21,9 +35,93 @@ describe('The Entrypoint Worker', () => {
 		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
 	});
 
+	it('refuses a wrong signature', async () => {
+		const body = 'text from discord';
+		const timestamp = '12345';
+		const { publicKey, signature: rightSignature } = makeSignature(body, timestamp);
+		const signature = destroySignature(rightSignature);
+
+		const request = new IncomingRequest('http://example.com', {
+			method: 'POST',
+			headers: {
+				'X-Signature-Ed25519': signature,
+				'X-Signature-Timestamp': timestamp,
+			},
+			body,
+		});
+		const env = { DISCORD_PUBLIC_KEY: publicKey };
+		// Create an empty context to pass to `worker.fetch()`.
+		const ctx = createExecutionContext();
+
+		const response = await worker.fetch(request, env, ctx);
+		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
+		await waitOnExecutionContext(ctx);
+
+		expect(response.status).toBe(401);
+		const respBody = await response.json();
+		expect(respBody).toHaveProperty('title', 'Unauthorized');
+		expect(respBody).toHaveProperty('detail', 'Your signature is invalid.');
+	});
+
+	it('refuses no signature header', async () => {
+		const body = 'text from discord';
+		const timestamp = '12345';
+		const { publicKey, signature } = makeSignature(body, timestamp);
+
+		const request = new IncomingRequest('http://example.com', {
+			method: 'POST',
+			headers: {
+				// 'X-Signature-Ed25519': signature,
+				'X-Signature-Timestamp': timestamp,
+			},
+			body,
+		});
+		const env = { DISCORD_PUBLIC_KEY: publicKey };
+		// Create an empty context to pass to `worker.fetch()`.
+		const ctx = createExecutionContext();
+
+		const response = await worker.fetch(request, env, ctx);
+		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
+		await waitOnExecutionContext(ctx);
+
+		expect(response.status).toBe(401);
+		const respBody = await response.json();
+		expect(respBody).toHaveProperty('title', 'Unauthorized');
+		expect(respBody).toHaveProperty('detail', 'Header key "X-Signature-Ed25519" is required but missing.');
+	});
+
+	it('refuses no timestamp header', async () => {
+		const body = 'text from discord';
+		const timestamp = '12345';
+		const { publicKey, signature } = makeSignature(body, timestamp);
+
+		const request = new IncomingRequest('http://example.com', {
+			method: 'POST',
+			headers: {
+				'X-Signature-Ed25519': signature,
+				// 'X-Signature-Timestamp': timestamp,
+			},
+			body,
+		});
+		const env = { DISCORD_PUBLIC_KEY: publicKey };
+		// Create an empty context to pass to `worker.fetch()`.
+		const ctx = createExecutionContext();
+
+		const response = await worker.fetch(request, env, ctx);
+		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
+		await waitOnExecutionContext(ctx);
+
+		expect(response.status).toBe(401);
+		const respBody = await response.json();
+		expect(respBody).toHaveProperty('title', 'Unauthorized');
+		expect(respBody).toHaveProperty('detail', 'Header key "X-Signature-Timestamp" is required but missing.');
+	});
+
 	it('returns an error when env var "DISCORD_PUBLIC_KEY" is missing', async () => {
 		const request = new IncomingRequest('http://example.com');
-		const { DISCORD_PUBLIC_KEY: _, ...env } = makeEnv();
+		// Make `env` by making an object satisfying Env and removing the field.
+		const fakeEnv: Env = { DISCORD_PUBLIC_KEY: '' };
+		const { DISCORD_PUBLIC_KEY: _, ...env } = fakeEnv;
 		// Create an empty context to pass to `worker.fetch()`.
 		const ctx = createExecutionContext();
 
@@ -38,8 +136,17 @@ describe('The Entrypoint Worker', () => {
 	});
 });
 
-function makeEnv(): Env {
-	return {
-		DISCORD_PUBLIC_KEY: 'DiscordPublicKeyForTest',
-	};
+function makeSignature(body: string, timestamp: string): { publicKey: string; signature: string } {
+	const message = body + timestamp;
+	const keyPair = sign.keyPair.fromSeed(SEED);
+	const publicKey = Buffer.from(keyPair.publicKey).toString('hex');
+	const signatureBytes = sign.detached(Buffer.from(message), keyPair.secretKey);
+	const signature = Buffer.from(signatureBytes).toString('hex');
+	return { publicKey, signature };
+}
+
+function destroySignature(signature: string): string {
+	const buf = Buffer.from(signature, 'hex');
+	buf[0] = buf[0] ^ 0xff;
+	return buf.toString('hex');
 }
